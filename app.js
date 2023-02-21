8ad11831ad29296d38596eb5deec9605013f64a0
Victor Olatunji <victoluolatunji@gmail.com>
2023-02-21 08:52:23 +0100

    dayy 2!

---
+++ app.js
@@ -0,0 +1,52 @@
+const prevBtn = document.querySelector('#prev');
+const nextBtn = document.querySelector('#next');
+const progress = document.querySelector('#progress');
+const circles = document.querySelectorAll('.circle');
+
+
+let currentActive = 1;
+
+
+nextBtn.addEventListener('click', () => {
+    currentActive++
+
+    if (currentActive > circles.length) {
+        currentActive = circles.length
+    }
+    update()
+})
+
+prevBtn.addEventListener('click', () => {
+    currentActive--
+
+    if (currentActive < 1) {
+        currentActive = 1
+    }
+
+    update()
+})
+
+function update() {
+    circles.forEach((circle, idx) => {
+        if (idx < currentActive) {
+            circle.classList.add('active')
+        } else {
+            circle.classList.remove('active')
+        }
+    });
+
+    const actives = document.querySelectorAll('.active')
+
+    progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%';
+
+    if (currentActive === 1) {
+        prevBtn.disabled = true
+    } else if (currentActive === circles.length) {
+        nextBtn.disabled = true
+    } else {
+        prevBtn.disabled = false
+        nextBtn.disabled = false
+    }
+}
+
+
